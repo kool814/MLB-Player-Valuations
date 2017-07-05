ec6a3b28a12f5e38028c21e24562ec30ea512ab8
kool814 <karthik.s814@gmail.com>
2017-07-05 13:10:18 -0700

    Parsing and seperating file

--- main.cpp
+++ main.cpp
@@ -2,4 +2,36 @@
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <vector>
 
+int main(){
+	std::vector <std::vector <std::string> > data;
+	std::ifstream infile( "2017_MLB_Player_Info.md" );
+
+	while (infile){
+		std::string s;
+	    if (!getline( infile, s )){
+	    	break;
+	    }
+
+	    std::istringstream ss( s );
+	    std::vector <std::string> record;
+
+	    while (ss){
+	      std::string s;
+	      if (!getline( ss, s, ',' )){
+	      	break;
+	      }
+	      record.push_back( s );
+	    }
+
+	    data.push_back( record );
+	}
+
+	std::cout<<"yes"<<std::endl;
+	for(int x=0; x<data.size(); x++){
+		std::cout<<data[x][1]<<std::endl;
+	}
+
+	return 0;
+}
