40712a7e5454b461a11bf4382379dfe58a12d4e5
kool814 <karthik.s814@gmail.com>
2017-07-07 17:48:21 -0700

    fixed player salary parsing bugs

--- main.cpp
+++ main.cpp
@@ -10,7 +10,10 @@
 
 int main(){
 	std::vector <std::vector <std::string> > data;
-	std::map<std::string, std::vector <std::vector <std::string> > > map_data;
+	std::map<std::string, std::vector <std::vector <std::string> > > map_player_salary;
+
+	std::vector <std::vector <std::string> > pitcher_data;
+	std::map<std::string, std::vector <std::vector <std::string> > > map_pitcher_player;
 
 	std::ifstream infile( "2017_MLB_Player_Salary_Info.md" );
 	std::ifstream infile_batter( "2017_MLB_Batter_Info.md" );
@@ -47,17 +50,55 @@ int main(){
 	    	x++;
 	    }
 
-		map_data.insert(std::make_pair(modified_name, data));
+		map_player_salary.insert(std::make_pair(modified_name, data));
 		count++;
 	}
 
+	//parse thru player pitcher info and store everything into a map
+	count=0;
+	while (infile_pitcher){
+		std::string s;
+	    if (!getline( infile_pitcher, s )){
+	    	break;
+	    }
+
+	    std::istringstream ss( s );
+	    std::vector <std::string> record;
+
+	    while (ss){
+	      std::string s;
+	      if (!getline( ss, s, ',' )){
+	      	break;
+	      }
+	      record.push_back( s );
+	    }
+
+	    pitcher_data.push_back( record );
+	    std::string full_name= pitcher_data[count][1];
+	    std::string modified_name="";
+	    int x=0;
+	    while(x<full_name.size()){
+	    	if(full_name[x]!='\\'){
+	    		modified_name.push_back(full_name[x]);
+	    	}else{
+	    		modified_name.pop_back();
+	    		break;
+	    	}
+	    	x++;
+	    }
+
+		map_pitcher_player.insert(std::make_pair(modified_name, pitcher_data));
+		count++;
+	}
+
+	//test if player salary data works 
 	for(int x=0; x<data.size(); x++){
 		if((data[x][21])!=""){
 			std::cout<<"Name: "<< data[x][1]<<" Salary: "<< data[x][21]<<std::endl;
 		}
 	}
 
-	if(map_data.find("Clayton Kershaw")!=map_data.end()){
+	if(map_player_salary.find("Clayton Kershaw")!=map_player_salary.end()){
 		std::cout<<"map works!!"<<std::endl;
 	}
 
