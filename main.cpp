13bf1ed6af5801e452c7daf3b05492c486880d02
kool814 <karthik.s814@gmail.com>
2017-07-26 18:41:53 -0700

    Not using parse_data function

--- main.cpp
+++ main.cpp
@@ -8,49 +8,45 @@
 #include <ctype.h>
 #include <map>
 
-std::vector <std::vector <std::string> > data;
-std::map<std::string, std::vector <std::vector <std::string> > > map_player_salary;
+// std::map<std::string, std::vector <std::vector <std::string> > >& parse_data(std::ifstream &instream_data){
+// 	int count=0;
+// 	while (instream_data){
+// 		std::string s;
+// 	    if (!getline( instream_data, s )){
+// 	    	break;
+// 	    }
+// 	    std::istringstream ss( s );
+// 	    std::vector <std::string> record;
 
-std::map<std::string, std::vector <std::vector <std::string> > >& parse_data(std::ifstream &instream_data){
-	int count=0;
-	//std::vector <std::vector <std::string> > data;
-	while (instream_data){
-		std::string s;
-	    if (!getline( instream_data, s )){
-	    	break;
-	    }
-	    std::istringstream ss( s );
-	    std::vector <std::string> record;
-
-	    while (ss){
-	      std::string s;
-	      if (!getline( ss, s, ',' )){
-	      	break;
-	      }
-	      record.push_back( s );
-	    }
-	    data.push_back( record );
-	    std::string full_name= data[count][1];
-	    std::string modified_name="";
-	    int x=0;
-	    while(x<full_name.size()){
-	    	if(full_name[x]!='\\'){
-	    		modified_name.push_back(full_name[x]);
-	    	}else{
-	    		modified_name.pop_back();
-	    		break;
-	    	}
-	    	x++;
-	    }
-		map_player_salary.insert(std::make_pair(modified_name, data));
-		count++;
-	}
-	return map_player_salary;
-}
+// 	    while (ss){
+// 	      std::string s;
+// 	      if (!getline( ss, s, ',' )){
+// 	      	break;
+// 	      }
+// 	      record.push_back( s );
+// 	    }
+// 	    data.push_back( record );
+// 	    std::string full_name= data[count][1];
+// 	    std::string modified_name="";
+// 	    int x=0;
+// 	    while(x<full_name.size()){
+// 	    	if(full_name[x]!='\\'){
+// 	    		modified_name.push_back(full_name[x]);
+// 	    	}else{
+// 	    		modified_name.pop_back();
+// 	    		break;
+// 	    	}
+// 	    	x++;
+// 	    }
+// 		map_player_salary.insert(std::make_pair(modified_name, data));
+// 		count++;
+// 	}
+// 	return map_player_salary;
+// }
 
 int main(){
-	// std::vector <std::vector <std::string> > data;
-	// std::map<std::string, std::vector <std::vector <std::string> > > map_player_salary;
+	std::vector <std::vector <std::string> > data;
+	std::map<std::string, std::vector <std::vector <std::string> > > map_player_salary;
 
 	std::vector <std::vector <std::string> > pitcher_data;
 	std::map<std::string, std::vector <std::vector <std::string> > > map_pitcher_player;
@@ -59,8 +55,8 @@ int main(){
 	std::ifstream infile_batter( "2017_MLB_Batter_Info.md" );
 	std::ifstream infile_pitcher( "2017_MLB_Pitcher_Info.md" );
 
-	map_player_salary= parse_data(infile);
-	/*
+	//map_player_salary= parse_data(infile);
+	
 	int count=0;
 	
 	while (infile){
@@ -95,7 +91,7 @@ int main(){
 		map_player_salary.insert(std::make_pair(modified_name, data));
 		count++;
 	}
-	*/
+	
 	//test if player salary data works 
 	for(int x=0; x<data.size(); x++){
 		if((data[x][21])!=""){
@@ -107,7 +103,7 @@ int main(){
 		std::cout<<"map works!!"<<std::endl;
 	}
 
-/*
+	//map_pitcher_player= parse_data(infile_pitcher);
 	
 	//parse thru player pitcher info and store everything into a map
 	count=0;
@@ -156,6 +152,5 @@ int main(){
 	if(map_pitcher_player.find("Mike Wright")!=map_pitcher_player.end()){
 		std::cout<<"map works!!"<<std::endl;
 	}
-	*/
 	return 0;
 }
