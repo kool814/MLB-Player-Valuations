4bc5013140bd5752d28526af4cd05f6998c101ce
kool814 <karthik.s814@gmail.com>
2017-07-29 19:52:28 -0700

    Took out redundant code

--- main.cpp
+++ main.cpp
@@ -56,90 +56,17 @@ int main(){
 
 	parse_data(infile, data, map_player_salary);
 	
-	int count=0;
-	
-	// while (infile){
-	// 	std::string s;
-	//     if (!getline( infile, s )){
-	//     	break;
-	//     }
-	//     std::istringstream ss( s );
-	//     std::vector <std::string> record;
-
-	//     while (ss){
-	//       std::string s;
-	//       if (!getline( ss, s, ',' )){
-	//       	break;
-	//       }
-	//       record.push_back( s );
-	//     }
-
-	//     data.push_back( record );
-	//     std::string full_name= data[count][1];
-	//     std::string modified_name="";
-	//     int x=0;
-	//     while(x<full_name.size()){
-	//     	if(full_name[x]!='\\'){
-	//     		modified_name.push_back(full_name[x]);
-	//     	}else{
-	//     		modified_name.pop_back();
-	//     		break;
-	//     	}
-	//     	x++;
-	//     }
-	// 	map_player_salary.insert(std::make_pair(modified_name, data));
-	// 	count++;
-	// }
-
 	//test if player salary data works 
 	for(int x=0; x<data.size(); x++){
 		if((data[x][21])!=""){
 			std::cout<<"Name: "<< data[x][1]<<" Salary: "<< data[x][21]<<std::endl;
 		}
 	}
-
 	if(map_player_salary.find("Clayton Kershaw")!=map_player_salary.end()){
 		std::cout<<"map works!!"<<std::endl;
 	}
 
 	parse_data(infile_pitcher, pitcher_data, map_pitcher_player);
-	
-	// //parse thru player pitcher info and store everything into a map
-	// count=0;
-	// while (infile_pitcher){
-	// 	std::string s;
-	//     if (!getline( infile_pitcher, s )){
-	//     	break;
-	//     }
-
-	//     std::istringstream ss( s );
-	//     std::vector <std::string> record;
-
-	//     while (ss){
-	//       std::string s;
-	//       if (!getline( ss, s, ',' )){
-	//       	break;
-	//       }
-	//       record.push_back( s );
-	//     }
-
-	//     pitcher_data.push_back( record );
-	//     std::string full_name= pitcher_data[count][1];
-	//     std::string modified_name="";
-	//     int x=0;
-	//     while(x<full_name.size()){
-	//     	if(full_name[x]!='\\'){
-	//     		modified_name.push_back(full_name[x]);
-	//     	}else{
-	//     		modified_name.pop_back();
-	//     		break;
-	//     	}
-	//     	x++;
-	//     }
-	// 	map_pitcher_player.insert(std::make_pair(modified_name, pitcher_data));
-	// 	count++;
-	// }
-
 
 	//test if pitcher player data works 
 	for(int x=0; x<pitcher_data.size(); x++){
@@ -147,7 +74,6 @@ int main(){
 			std::cout<<"Name: "<< pitcher_data[x][1]<<" ERA: "<< pitcher_data[x][8]<<std::endl;
 		}
 	}
-
 	if(map_pitcher_player.find("Mike Wright")!=map_pitcher_player.end()){
 		std::cout<<"map works!!"<<std::endl;
 	}
