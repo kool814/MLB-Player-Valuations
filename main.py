1347d9ecdd418cca7b9e4618faa2c7dd7929b764
argentinoj <argentinoj.0.2.0.4@gmail.com>
2018-02-13 16:53:58 -0500

    Beginning Parsing
    Began Parsing and described the meaning of the abbreviations

--- main.py
+++ main.py
@@ -1,5 +1,43 @@
 #This is the file that we will use to parse the Batter, Pitcher, and Salary data
 
-#This is a test edit for my branch
+f = open("2017_MLB_Batter_Info.md")
+data = []
+for line in f:
+    data.append(line.split(','))
+    
+f.close()
 
-#test multiple branches
+for item in data[0]:
+    print(item)
+"""
+Rk = rank
+Name
+Age
+Tm = Team
+Lg = League
+G = Games Played
+PA = Plate Appearance
+AB = At-bat
+R = Runs
+H = Hits
+2B = Double
+3B = Triple
+HR = Home Run
+RBI = Runs Batted In
+SB = Stolen Base
+CS = Caught Stealing
+BB = Walk
+SO = Strikeout
+BA = Batting Average
+OBP = On-Base Percentage
+SLG = Slugging Percentage
+OPS = On Base Plus Slugging
+OPS+ = On Base Plus Slugging Plus
+TB = Total Bases
+GDP = Grounded Into Double Play
+HBP = Hit By Pitch
+SH = Sacrifice Bunt
+SF = Sacrifice Fly
+IBB = Intentional Walk
+Pos Summary 
+"""
