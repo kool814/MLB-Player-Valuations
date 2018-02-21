f87f80aea68862c29d1eb59d59eb6a0109a55492
kool814 <karthik.s814@gmail.com>
2018-02-20 20:28:01 -0500

    Started parsing salary data

--- main.py
+++ main.py
@@ -2,3 +2,27 @@
 
 #Dictionary with key: Player and value: Dictionary containing key:Team and value: stats 
 
+import math
+
+def parse_salary_data(dict, salary_data):
+    '''
+    Given the salary data info, parse the info and store the player, team, 
+    and yearly salary data.
+    '''
+    data = open(salary_data, "r")
+
+    with open(salary_data) as data_parse:
+        for line in data_parse:
+            print(line)
+
+
+if __name__ == "__main__":
+    salary_data_dict = {}
+    salary_data_dict['player'] = {}
+    salary_data_dict['player']['team'] = 'stats'
+    print(salary_data_dict)
+    print("this is a test")
+
+    parse_salary_data(salary_data_dict, 
+                "/Users/karthiksuresh/Documents/GitHub/MLB-Player-Valuations/2017_MLB_Player_Salary_Info.md")
+    
