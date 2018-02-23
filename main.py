5ba70cc7e7de18eec82bb5e79d70817c65790d55
kool814 <karthik.s814@gmail.com>
2018-02-23 16:30:58 -0500

    Cleaning player name

--- main.py
+++ main.py
@@ -9,11 +9,28 @@ def parse_salary_data(dict, salary_data):
     Given the salary data info, parse the info and store the player, team, 
     and yearly salary data.
     '''
-    data = open(salary_data, "r")
+    #data = open(salary_data, "r")
 
     with open(salary_data) as data_parse:
         for line in data_parse:
-            print(line)
+            line = line.strip().split(",")
+
+            player_name_split_space=line[1].split()
+            player_name=player_name_split_space[0]
+
+            if(len(player_name_split_space)>=2):
+                player_name +=" "
+                player_name +=player_name_split_space[1]
+            
+            team= line[4]
+            salary= line[22]
+            
+            if player_name in dict:
+                ''' if the player is already in the dictionary, then add them to another team '''
+            else:
+                ''' add them to the dictionary'''
+
+            print(player_name)
 
 
 if __name__ == "__main__":
@@ -21,7 +38,6 @@ if __name__ == "__main__":
     salary_data_dict['player'] = {}
     salary_data_dict['player']['team'] = 'stats'
     print(salary_data_dict)
-    print("this is a test")
 
     parse_salary_data(salary_data_dict, 
                 "/Users/karthiksuresh/Documents/GitHub/MLB-Player-Valuations/2017_MLB_Player_Salary_Info.md")
